ebc3a12ea674ebf2b99650a82a156eae56b3aff4
penguin-teal <_penguin@tuta.io>
2025-05-18 23:36:21 -0500

    Add RoomManager for list of active rooms

---
+++ Riffle/Services/RoomManager.cs
@@ -0,0 +1,24 @@
+﻿// Copyright 2025 Roman Lefler
+
+using Riffle.Models;
+using System.Collections.Concurrent;
+
+namespace Riffle.Services
+{
+    public class RoomManager
+    {
+        public static ConcurrentDictionary<string, Room> Rooms { get; } = [];
+        public static ConcurrentDictionary<string, Room> HostToRoom { get; } = [];
+
+        public static bool AddRoom(Room room)
+        {
+            return Rooms.TryAdd(room.JoinCode, room) && HostToRoom.TryAdd(room.HostConnectionId, room);
+        }
+
+        public static bool RemoveRoom(Room room)
+        {
+            return Rooms.TryRemove(room.JoinCode, out _) && Rooms.TryRemove(room.HostConnectionId, out _);
+        }
+
+    }
+}
