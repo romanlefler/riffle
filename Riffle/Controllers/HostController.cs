18d06e05c0ec77509d1891ec58dab96703c7aaaf
penguin-teal <_penguin@tuta.io>
2025-05-18 23:12:39 -0500

    Page to host roundabout game with javascript file

---
+++ Riffle/Controllers/HostController.cs
@@ -0,0 +1,23 @@
+﻿// Copyright 2025 Roman Lefler
+
+using Microsoft.AspNetCore.Mvc;
+using Riffle.Utilities;
+
+namespace Riffle.Controllers
+{
+    public class HostController : Controller
+    {
+        [HttpGet("/Host")]
+        public IActionResult Host()
+        {
+            return View();
+        }
+
+        [HttpGet("/Host/Roundabout")]
+        public IActionResult Roundabout()
+        {
+            return View("Roundabout");
+        }
+
+    }
+}
