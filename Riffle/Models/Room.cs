38ff51b15059eb0cf5a913b6c1d4183f0953c0b6
penguin-teal <_penguin@tuta.io>
2025-05-18 23:36:05 -0500

    Add room object

---
+++ Riffle/Models/Room.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2025 Roman Lefler
+
+using Riffle.Utilities;
+
+namespace Riffle.Models
+{
+    public class Room
+    {
+
+        public string JoinCode { get; }
+
+        public string HostConnectionId { get; }
+
+        public GameType Game { get; }
+
+        public HashSet<string> Participants { get; }
+
+        public Room(string hostConnectionId, GameType game)
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(hostConnectionId, nameof(hostConnectionId));
+
+            string? code = Utilities.JoinCode.Create();
+            if (code == null) throw new TooManyRoomsException();
+            JoinCode = code;
+
+            HostConnectionId = hostConnectionId;
+            Game = game;
+            Participants = [];
+        }
+    }
+}
