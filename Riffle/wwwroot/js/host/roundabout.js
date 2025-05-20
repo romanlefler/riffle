64b491ce57627aeabed383a5ffca197053fa1c85
penguin-teal <_penguin@tuta.io>
2025-05-19 20:04:04 -0500

    Play button with timer for host

--- Riffle/wwwroot/js/host/roundabout.js
+++ Riffle/wwwroot/js/host/roundabout.js
@@ -2,6 +2,12 @@
 
 const conn = new signalR.HubConnectionBuilder().withUrl("/roomHub").build();
 var joinCode = null;
+var users = [];
+
+function showUsers() {
+    const list = document.getElementById("playerList");
+    list.textContent = users.map(u => u.name).join(", ");
+}
 
 conn.on("RoomCreated", givenCode => {
     console.log("Room created: " + givenCode);
@@ -12,11 +18,37 @@ conn.on("RoomCreated", givenCode => {
 conn.on("RoomError", msg => {
     console.log("Room error: " + msg);
     document.getElementById("joinCode").textContent = `Error: ${msg}`;
-})
+});
+
+conn.on("UserJoined", (id, name) => {
+    users.push({ id, name });
+    showUsers();
+});
+
+conn.on("UserLeft", id => {
+    users = users.filter(u => u.id === id);
+    showUsers();
+});
 
 conn.start().catch(console.error).then(() => {
 
-    conn.invoke("CreateRoom", 1).catch(console.error);
+    conn.invoke("CreateRoom", 1);
+
+    const playBtn = document.getElementById("playButton");
+    const playTimer = document.getElementById("playTimer");
+    console.log(playBtn);
+    playBtn.addEventListener("click", () => {
+        var playClickTime = Date.now();
+        var intervalId = setInterval(() => {
+            const deltaS = Math.ceil(4.0 - (Date.now() - playClickTime) / 1000);
+            if (deltaS <= 0.0) {
+                clearInterval(intervalId);
+                playTimer.textContent = "";
+                conn.invoke("StartGame");
+            }
+            else playTimer.textContent = String(deltaS);
+        }, 1000);
+    });
 
 });
 
