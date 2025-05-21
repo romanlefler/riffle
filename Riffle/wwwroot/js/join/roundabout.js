bcc051406a5c5491a0858aea3121a3a0b099d65b
Roman Lefler <romanlefler@proton.me>
2025-05-20 22:27:19 -0500

    Fix typo in document and make screen say Connecting until connected

--- Riffle/wwwroot/js/join/roundabout.js
+++ Riffle/wwwroot/js/join/roundabout.js
@@ -4,6 +4,16 @@
 // const joinCode is the join code
 // const plName is the player name
 
+const lobbyScreen = document.getElementById("lobbyScreen");
+const guessScreen = document.getElementById("guessScreen");
+
+const guessBox = document.getElementById("guessBox");
+
+function hideAllScreens() {
+    lobbyScreen.style.display = "none";
+    guessScreen.style.display = "none";
+}
+
 conn.on("RoomError", msg => {
     console.log("Room error: " + msg);
     document.body.textContent += `Error: ${msg}`;
@@ -11,9 +21,14 @@ conn.on("RoomError", msg => {
 
 conn.on("GameStarted", () => {
     console.log(`Game ${joinCode} started as ${plName}.`);
+
+    hideAllScreens();
+    guessScreen.style.display = "block";
 });
 
 conn.start().catch(console.error).then(() => {
+
+    document.getElementById("connectingText").textContent = "You're in!";
 
     conn.invoke("JoinRoom", joinCode, plName);
 
