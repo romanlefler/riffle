61a09f9c61554d4a73d6969581fe5a0d82840fc9
Roman Lefler <romanlefler@proton.me>
2025-05-24 16:33:38 -0500

    Member / Roundabout: Guess word and end game screens

--- Riffle/wwwroot/js/join/roundabout.js
+++ Riffle/wwwroot/js/join/roundabout.js
@@ -7,16 +7,21 @@
 const lobbyScreen = document.getElementById("lobbyScreen");
 
 const choiceScreen = document.getElementById("choiceScreen");
+const choiceMsg = document.getElementById("choiceMsg");
 const choiceBox = document.getElementById("choiceBox");
 const choiceSubmit = document.getElementById("choiceSubmit");
 
 const chosenScreen = document.getElementById("chosenScreen");
 const chosenText = document.getElementById("chosenText");
 
+const endScreen = document.getElementById("endScreen");
+const endMsg = document.getElementById("endMsg");
+
 function hideAllScreens() {
     lobbyScreen.style.display = "none";
     choiceScreen.style.display = "none";
     chosenScreen.style.display = "none";
+    endScreen.style.display = "none";
 }
 
 conn.on("RoomError", msg => {
@@ -26,20 +31,39 @@ conn.on("RoomError", msg => {
 
 conn.on("GameStarted", () => {
     console.log(`Game ${joinCode} started as ${plName}.`);
-
-    hideAllScreens();
-    choiceScreen.style.display = "block";
+    beginChoosing();
 });
 
-choiceSubmit.addEventListener("click", () => {
-
+function submitChoice() {
+    choiceSubmit.removeEventListener("click", submitChoice);
     choiceBox.disabled = true;
     choiceSubmit.disabled = true;
     conn.invoke("StringMsg", "ChooseWord", choiceBox.value);
-});
+}
+
+function beginChoosing() {
+    choiceSubmit.addEventListener("click", submitChoice);
+    choiceMsg.textContent = "Choose your secret word!";
+
+    hideAllScreens();
+    choiceScreen.style.display = "block";
+}
+
+function submitGuess() {
+    conn.invoke("StringMsg", "GuessWord", choiceBox.value);
+    choiceBox.value = "";
+}
+
+function beginGuessing() {
+    choiceSubmit.addEventListener("click", submitGuess);
+    choiceMsg.textContent = "Guess the secret word!";
+
+    hideAllScreens();
+    choiceScreen.style.display = "block";
+}
 
 conn.on("ChoiceAccepted", () => {
-
+    choiceBox.value = "";
     const msgs = [ "Good word!", "Not what I would've chosen", "Interesting choice..." ];
     chosenText.textContent = msgs[Math.floor(Math.random() * 3)];
 
@@ -47,7 +71,25 @@ conn.on("ChoiceAccepted", () => {
 
     choiceBox.disabled = false;
     choiceSubmit.disabled = false;
+    choiceSubmit.removeEventListener("click", submitChoice);
     chosenScreen.style.display = "block";
+});
+
+conn.on("GuessingStarted", beginGuessing);
+
+conn.on("SuccessfulGuess", () => {
+    hideAllScreens();
+});
+
+conn.on("GameEnded", (connIds) => {
+    hideAllScreens();
+    endScreen.style.display = "block";
+
+    const myConnId = conn.connection?.connectionId;
+    if (!connIds || !myConnId) return;
+
+    const placing = connIds.indexOf(myConnId);
+    if (placing > -1) endMsg.textContent = `#${placing + 1}`;
 });
 
 conn.start().catch(console.error).then(() => {
