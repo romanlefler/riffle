7ec0f843610615db9bcc35928430fbc4da2aa5b5
Roman Lefler <romanlefler@proton.me>
2025-05-24 17:46:01 -0500

    Create separate frontend project that builds to backend's static js files in wwwroot/dist

---
+++ RiffleFrontend/README.md
@@ -0,0 +1,12 @@
+
+# Install Dependencies
+
+```shell
+npm install typescript vite gsap @types/node
+```
+
+# Build to Backend's Static Files
+
+```shell
+npx vite build
+```
---
+++ RiffleFrontend/vite.config.ts
@@ -0,0 +1,14 @@
+﻿import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+    build: {
+        outDir: "../Riffle/wwwroot/dist",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                roundaboutHost: resolve(__dirname, "frontend/roundaboutHost.ts")
+            }
+        }
+    }
+});
