06a474b5341872af7aac8f5d4d3d04b860e5f44e
Roman Lefler <romanlefler@proton.me>
2025-05-25 14:52:49 -0500

    Host / Roundabout: Script that create a room and shows join code

--- RiffleFrontend/src/roundaboutHost.ts
+++ RiffleFrontend/src/roundaboutHost.ts
@@ -1,1 +1,23 @@
-﻿
+﻿import { GameType } from "./defines";
+import { RoomManHost } from "./roomManHost";
+
+const room: RoomManHost = new RoomManHost(GameType.Roundabout);
+
+function showUsers() {
+    console.log(String(room.users));
+}
+
+async function main() {
+
+    room.addUserJoinedListener(showUsers);
+    room.addUserLeftListener(showUsers);
+
+    await room.connect();
+    await room.createRoom();
+
+    console.log(room.joinCode);
+    document.getElementById("join-code")!.textContent = room.joinCode;
+
+}
+
+main();
