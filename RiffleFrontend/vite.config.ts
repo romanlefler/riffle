ae2677da6afe937cf1958dc4b31dcb55307f4c85
Roman Lefler <romanlefler@proton.me>
2025-05-25 14:57:30 -0500

    Copy Vite manifest to backend

--- RiffleFrontend/vite.config.ts
+++ RiffleFrontend/vite.config.ts
@@ -1,14 +1,31 @@
 ﻿import { defineConfig } from "vite";
 import { resolve } from "path";
+import { renameSync } from "fs";
+
+const outDir = "../Riffle/wwwroot/dist";
 
 export default defineConfig({
     build: {
-        outDir: "../Riffle/wwwroot/dist",
+        outDir: resolve(__dirname, outDir),
+        manifest: "viteMap.json",
+        sourcemap: true,
         emptyOutDir: true,
         rollupOptions: {
             input: {
                 roundaboutHost: resolve(__dirname, "src/roundaboutHost.ts")
             }
         }
-    }
+    },
+    cacheDir: resolve(__dirname, ".vite"),
+    plugins: [
+        {
+            name: "move-manifest",
+            async writeBundle() {
+                const manifest = resolve(__dirname, outDir, "viteMap.json");
+                const target = resolve(__dirname, "../Riffle/Resources/viteMap.json");
+
+                renameSync(manifest, target);
+            }
+        }
+    ]
 });
