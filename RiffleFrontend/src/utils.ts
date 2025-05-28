8ff991a907287e36933190a1089b19eb9223e583
Roman Lefler <romanlefler@proton.me>
2025-05-27 21:14:08 -0500

    Frontend: Max wait timeout utility

--- RiffleFrontend/src/utils.ts
+++ RiffleFrontend/src/utils.ts
@@ -4,3 +4,17 @@ export async function sleep(seconds : number) : Promise<void> {
         setTimeout(resolve, seconds * 1000.0);
     });
 }
+
+/**
+ * Resolves/rejects the given promise unless a certain amount
+ * of time passes before, where it will reject with the message
+ * "Timed out."
+ * @param promise The promise to try
+ * @param maxSeconds THe seconds to give before rejection
+ */
+export async function maxWait<T>(promise : Promise<T>, maxSeconds : number) : Promise<T> {
+    return Promise.race([
+        promise,
+        new Promise((_, reject) => sleep(maxSeconds).then(() => reject("Timed out.")))
+    ]) as Promise<T>;
+}
